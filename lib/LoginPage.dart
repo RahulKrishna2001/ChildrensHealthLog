3c7e1f8299d58352c9d6010225e76373abaf67f4
RAHUL KRISHNA <rahulkrishnaparagi@gmail.com>
2022-03-25 08:11:50 +0530

    Update LoginPage.dart

--- lib/LoginPage.dart
+++ lib/LoginPage.dart
@@ -4,7 +4,6 @@ import 'package:childrenshealthlog/Start.dart';
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:firebase_auth/firebase_auth.dart';
-import './AppInfo.dart';
 
 class LoginPage extends StatefulWidget {
   @override
